95df054e78d88619fd16d90ddcf225a6692db0c8
GAMING CHAUHAN <143026011+Gamingbuddyyy@users.noreply.github.com>
2023-10-19 19:17:53 +0530

    Update alive.py

--- SpamX/modules/for_bot/alive.py
+++ SpamX/modules/for_bot/alive.py
@@ -47,7 +47,7 @@ async def SpamX_(SpamX: Client, message: Message):
     Mai = await SpamX.get_me()
     chat = message.chat
     info = await message.reply_text("processing......")
-    spamx_reply = "**__SpamX by RiZoeLX;__** \n\n"
+    spamx_reply = "**__TomanX by Toman;__** \n\n"
     spamx_reply += "<=======================> \n"
 
     try:
@@ -89,9 +89,9 @@ async def SpamX_(SpamX: Client, message: Message):
     spamx_reply += f"**Battery:** {battery} \n\n"
     await info.edit_text("..!......")
 
-    spamx_reply += f"**SpamX Version:** `{__version__}` \n"
+    spamx_reply += f"**TomanX Version:** `{__version__}` \n"
     spamx_reply += f"**Python Version:** `{platform.python_version()}` \n"
-    spamx_reply += f"**pyRiZoeLX Version:** `{rizoelx_vr}` \n"
+    spamx_reply += f"**pyToman Version:** `{rizoelx_vr}` \n"
     spamx_reply += f"**Pyrogram Version:** `{pyrogram_vr}` \n\n"
     
     spamx_reply += f"**Your Name:** {user.first_name} \n"
@@ -107,7 +107,7 @@ async def SpamX_(SpamX: Client, message: Message):
        spamx_reply += f"**Total Gbanned users:** `{gban_db.gban_count()}` \n"
     spamx_reply += "<=======================> \n\n"
     spamx_reply += "**Source Code:** [GitHub 🐈‍⬛](https://github.com/RiZoeLX/SpamX) \n\n"
-    spamx_reply += "**© @RiZoeLX** || Support: @DNHxHELL"
+    spamx_reply += "**© @sahenshah01** || Support: @billaganghh"
     
     try:
        await info.edit_text(spamx_reply, disable_web_page_preview=True)
